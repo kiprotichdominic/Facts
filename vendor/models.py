aa1c04d871f26010e6d04706d965d4dc235cda85
peter kuria <you@example.com>
2020-03-03 17:21:01 -0500

    imported post save and receiver

--- vendor/models.py
+++ vendor/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -14,7 +16,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()    
+    instance.profile.save()
 
 class Categories(models.Model):
 
