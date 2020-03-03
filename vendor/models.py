fac731fe201e17b3d5397a8e77f354e9a8d16fd4
peter kuria <you@example.com>
2020-03-03 17:07:06 -0500

    added profile,product and categories models

--- vendor/models.py
+++ vendor/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name=charfield(max_lenth=20)
+    email=models.EmailField()
+    phone=models.IntegerField()
+
+class Categories(models.Model):
+
+    PRODUCT_CHOICES = (
+        ("agriculture", "agriculture"),
+        ("electronics", "electronics"),
+        ("cars", "cars"),
+        ("utensils", "utensils"),
+
+    )
+
+
+    choice=models.charfield( chioces= PRODUCT_CHOICES)
+
+
+class Products(models.Model):
+    product_Name=models.charfield(max_lenth=20)
+    category=Foreignkey(Category)
+    quantity=models.IntegerField()
+    location = models.CharField(max_length=30, blank=True)
+    date_Posted= models.DateField(null=True, blank=True)
