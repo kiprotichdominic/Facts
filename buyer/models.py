75a5d5d17a0a960b201e7ce20b4378c76545730e
peter kuria <you@example.com>
2020-03-04 10:01:05 -0500

    added oders model

--- buyer/models.py
+++ buyer/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
 class buyer(models.Model):
-    user=OneToOneField(User)
+    # user=OneToOneField(User)
     email=models.EmailField()
 
 
@@ -10,8 +10,8 @@ class buyer(models.Model):
         return self.user.username
 
 class Order(models.Model):
-    product_Name=models.CharField()
+    product_Name=models.CharField(max_length=200)
     Price=models.DecimalField(max_digits=5, decimal_places=2)
-    orders=Foreignkey(buyer,related_name='orders')
+    orders=models.ForeignKey(buyer,on_delete=models.CASCADE,related_name='orders')
     def __str__(self):
         return self.product_Name
