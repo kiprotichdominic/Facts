f8ac67d2f0aaa21fcba00e19487dfde2b54c2389
mukhtarabdirahman <mukhtarabdirahmamn@gmail.com>
2020-03-04 11:54:10 +0300

    class order

--- buyer/models.py
+++ buyer/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 from datetime import datetime
+
+
+
+class Order(models.Model):
+    '''
+    The Order model represents a customer order. It includes a
+    ManyToManyField of products the customer is ordering and stores
+    the date and total price information.
+    '''
+    customer = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    status_code = models.ForeignKey('StatusCode', on_delete=models.CASCADE)
+    date_placed = models.DateTimeField(default=datetime.now)
+    total_price = models.DecimalField(max_digits=7, decimal_places=2)
+    comments = models.TextField(blank=True)
+    products = models.ManyToManyField('vendor.Products')
