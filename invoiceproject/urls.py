b3569c2a1aa773fb6c5700fb02d14db349eb79eb
skimanikings <kimanisimon856@gmail.com>
2020-03-05 10:52:26 +0300

    create a redirect route for the logout page

--- invoiceproject/urls.py
+++ invoiceproject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', include('buyer.urls')),
@@ -22,4 +24,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('logout/',auth_views.LogoutView.as_view(template_name="index.html"), name="logout"),
 ]
